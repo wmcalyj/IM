5b9944a836c3391e34e1faecfd3a9b94310dd38a
wmcalyj <wmcalyj@hotmail.com>
2013-12-21 22:31:48 -0600

    XML

--- InstantMessage/src/im/xmlservice/DefaultXMLService.java
+++ InstantMessage/src/im/xmlservice/DefaultXMLService.java
@@ -40,3 +40,5 @@ public class DefaultXMLService {
 		return map;
 	}
 }
+
+
--- InstantMessage/src/im/xmlservice/XMLConstants.java
+++ InstantMessage/src/im/xmlservice/XMLConstants.java
@@ -8,3 +8,4 @@ public class XMLConstants {
 	static final public String ITEM = "item";
 	static final public String CONFIG = "config";
 }
+
