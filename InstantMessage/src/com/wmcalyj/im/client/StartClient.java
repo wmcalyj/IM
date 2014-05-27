eac4298d2d749af90139b2a51a965f1a005bbaa4
mengchaow <wmcalyj@hotmail.com>
2014-05-27 17:13:54 -0500

    Move run client class to com.wmcalyj.im.client.StartClient

---
+++ InstantMessage/src/com/wmcalyj/im/client/StartClient.java
@@ -0,0 +1,20 @@
+package com.wmcalyj.im.client;
+
+import java.io.IOException;
+
+import com.wmcalyj.im.client.gui.LoginGui;
+
+public class StartClient {
+	public static void main(String[] args) throws IOException {
+		LoginGui login = new LoginGui();
+		login.Login();
+		// EncryptionService es = new EncryptionService();
+		// es.testEncrypt("���������������������������������������������������������������fuck������������������������");
+		// FakeWebservice fws = new FakeWebservice();
+		// // DefaultMessageService ws = new DefaultMessageService();
+		//
+		// // Test client side server
+
+	}
+
+}
